8b577bc83259663ada218a9e2bacce91b1bd0534
ANAND015 <anands.22cse@kongu.edu>
2025-01-02 21:49:13 +0530

    Task_of_Day@6

--- Backend/index.js
+++ Backend/index.js
@@ -1,23 +1,22 @@
-const express=require("express");
-var app=express();
-const PORT=3001;
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3001;
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to Backend Server")
+// Serve static files from the specified directory
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Backend Server");
+});
+app.get("/static",(req,res)=>{
+    res.sendFile("C:/Advanced Full Stack Training/Backend/public/index.html")
 });
 
-app.get('/json',(req,res)=>{
-    res.json({server:"Welcome to Backend",url:"localhost",port:{ PORT}})
+app.get('/json', (req, res) => {
+    res.json({ server: "Welcome to Backend", url: "localhost", port: PORT });
 });
 
-app.get('/static',(req,res)=>{
-    res.sendFile("C:/Advanced Full Stack Training/Backend/index.html")
+app.listen(PORT, () => {
+    console.log(`Backend Server Started \n Url: http://localhost:${PORT}`);
 });
-app.get('/next',(req,res)=>{
-    res.sendFile("C:/Advanced Full Stack Training/Backend/next.html")
-});
-
-app.listen(PORT,()=>{
-    console.log(`Backend Server Started \n Url:http://localhost:${PORT}`)
-});
-
