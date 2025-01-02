391891b83e9b35e2bbca5b79212d8b51c2d3ed98
ANAND015 <anands.22cse@kongu.edu>
2025-01-02 22:11:14 +0530

    Task_of_Day@6 change

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,6 @@ const path = require("path");
 const app = express();
 const PORT = 3001;
 
-// Serve static files from the specified directory
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
