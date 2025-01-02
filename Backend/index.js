bb8f2375cd9879454e7d9c2101f4c35b5cf87f87
ANAND015 <anands.22cse@kongu.edu>
2025-01-02 21:22:49 +0530

    Task of Day@6

--- Backend/index.js
+++ Backend/index.js
@@ -10,9 +10,12 @@ app.get('/json',(req,res)=>{
     res.json({server:"Welcome to Backend",url:"localhost",port:{ PORT}})
 });
 
-// app.get('/static',(req,res)=>{
-//     res.sendFile("C:\Advanced Full Stack Training\Backend\index.html")
-// });
+app.get('/static',(req,res)=>{
+    res.sendFile("C:/Advanced Full Stack Training/Backend/index.html")
+});
+app.get('/next',(req,res)=>{
+    res.sendFile("C:/Advanced Full Stack Training/Backend/next.html")
+});
 
 app.listen(PORT,()=>{
     console.log(`Backend Server Started \n Url:http://localhost:${PORT}`)
